13916258de824ef71e383b378ce9aec0c6460bdb
osirbu <SecurityPAssword240601>
2019-10-23 10:02:20 +0300

    Initial commit

---
+++ src/test/java/com/rakuten/campaign/CampaignApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rakuten.campaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
